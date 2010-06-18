31dd644d44fd587fa2e9775d5922baedb5755af8
Daniel Baumann <daniel@debian.org>
2010-06-18 19:46:16 +0200

    Also using an entity for the current year in the copyright year.

--- manual/bin/update-version.sh
+++ manual/bin/update-version.sh
@@ -4,9 +4,11 @@ set -e
 
 DATE="$(LC_ALL=C date +%Y-%m-%d)"
 VERSION="$(cat ../VERSION)"
+YEAR="$(LC_ALL=C date +%Y)"
 
 echo "Updating version information..."
 
 sed -i  -e "s|<!ENTITY pubdate.*$|<!ENTITY pubdate \"${DATE}\">|" \
-	-e "s|<!ENTITY version.*$|<!ENTITY version \"${VERSION}\">|" \
+	-e "s|<!ENTITY releaseinfo.*$|<!ENTITY releaseinfo \"${VERSION}\">|" \
+	-e "s|<!ENTITY year.*$|<!ENTITY year \"${YEAR}\">|" \
 en/index.xml

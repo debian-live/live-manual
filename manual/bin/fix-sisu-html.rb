186074a261583ed1c0fa0338bf8e9c4662ccc180
Daniel Baumann <daniel@debian.org>
2012-04-03 20:43:27 +0200

    Correcting indenting in fix-sisu-html.rb.

--- manual/bin/fix-sisu-html.rb
+++ manual/bin/fix-sisu-html.rb
@@ -7,13 +7,13 @@ input_file=output_file+"~"
 
 File.rename(output_file,input_file)
 File.open(output_file,"w") do |o|
-    doc=Nokogiri::HTML(open input_file)
-    # CSS3 selectors don't support regexes, so we take a shotgun approach,
-    # removing all tables with summaries (OK for current sisu output).
-    # Change to use a custom pseudo class if anything more refined is needed.
-    doc.css(%[table[summary]]).remove
-    toc=doc.css(%[h2,h4[class$="toc"]]).each do |node|
-	node.remove if node.inner_text.match(/Metadata|Manifest|SiSU/)
-    end
-    o.puts doc.to_html
+	doc=Nokogiri::HTML(open input_file)
+	# CSS3 selectors don't support regexes, so we take a shotgun approach,
+	# removing all tables with summaries (OK for current sisu output).
+	# Change to use a custom pseudo class if anything more refined is needed.
+	doc.css(%[table[summary]]).remove
+	toc=doc.css(%[h2,h4[class$="toc"]]).each do |node|
+		node.remove if node.inner_text.match(/Metadata|Manifest|SiSU/)
+	end
+	o.puts doc.to_html
 end

23df57f74701c34785366326abec1c56f1b5ebe9
Daniel Baumann <daniel@debian.org>
2012-06-03 12:28:33 +0200

    Automatically translating date formats when setting them during processing sisu files.

--- manual/bin/update-version.sh
+++ manual/bin/update-version.sh
@@ -2,11 +2,19 @@
 
 set -e
 
-DATE="$(LC_ALL=C date +%Y-%m-%d)"
 VERSION="$(cat ../VERSION)"
+
+DAY="$(LC_ALL=C date +%d)"
+MONTH="$(LC_ALL=C date +%m)"
 YEAR="$(LC_ALL=C date +%Y)"
 
 echo "Updating version information..."
-sed -i  -e "s|^ :published:.*$| :published: ${DATE}|" \
+sed -i  -e "s|^ :published:.*$| :published: ${YEAR}-${MONTH}-${DAY}|" \
 	-e "s|(C) 2006-.*|(C) 2006-${YEAR} Debian Live Project|" \
 en/live-manual.ssm
+
+if [ -e po/de/live-manual.ssm.po ]
+then
+	sed -i -e "s|:published: .*.${YEAR}|:published: ${DAY}.${MONTH}.${YEAR}|" \
+	po/de/live-manual.ssm.po
+fi

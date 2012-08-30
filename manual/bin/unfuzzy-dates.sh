ee9dd649ab24129cf9181e21a8df94e87920a9c1
chals <chals@altorricon.com>
2012-08-30 14:00:12 +0200

    Changing sed separatorsto follow the project's coding style and clarifying the echoed message.

--- manual/bin/unfuzzy-dates.sh
+++ manual/bin/unfuzzy-dates.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 set -e
 
@@ -9,11 +9,11 @@ XY="50"
 
 # Getting the unfuzzying done.
 
-echo "Fixing 'fuzzy' in date strings..."
+echo "Fixing 'fuzzy' in date strings if necessary..."
 
 for LANGUAGE in $(ls po)
 do
-	if [ "$(grep --line-number --word-regexp 'fuzzy' po/${LANGUAGE}/live-manual.ssm.po | sed 's/[^0-9]*//g')" -eq "${XY}" ] > /dev/null 2>&1
+	if [ "$(grep --line-number --word-regexp 'fuzzy' po/${LANGUAGE}/live-manual.ssm.po | sed 's|[^0-9]*||g')" -eq "${XY}" ] > /dev/null 2>&1
 	then
 		sed -i	-e "${XY} s|#, fuzzy, no-wrap|#, no-wrap|" \
 			-e "$((${XY} + 1)),$((${XY} + 3))d" \
